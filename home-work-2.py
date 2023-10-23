dd799d1a19f045902dfd112b2216a531348a03e9
skulakovskyi <skulakovskyi@skulakovskyi-mbp.netis>
2023-10-23 22:42:22 +0300

    Add home work 2

---
+++ home-work-2.py
@@ -0,0 +1,68 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        if len(value) != 10:
+            raise ValueError("The phone number must have 10 digits")
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone_number):
+        phone = Phone(phone_number)
+        self.phones.append(phone)
+
+    def remove_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                self.phones.remove(phone)
+                return
+        raise ValueError("Phone number not found")
+
+    def edit_phone(self, old_phone_number, new_phone_number):
+        for phone in self.phones:
+            if phone.value == old_phone_number:
+                phone.value = new_phone_number
+                return
+        raise ValueError("Phone number not found")
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone
+        raise ValueError("Phone number not found")
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(phone.value for phone in self.phones)}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        if name in self.data:
+            return self.data[name]
+        return None
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
