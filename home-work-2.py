2686d710c3d44f7b1193b006c56843ff95a992e6
skulakovskyi <skulakovskyi@skulakovskyi-mbp.netis>
2023-10-24 21:58:51 +0300

    Improve phone number validation

--- home-work-2.py
+++ home-work-2.py
@@ -17,7 +17,7 @@ class Name(Field):
 class Phone(Field):
     def __init__(self, value):
         super().__init__(value)
-        if len(value) != 10:
+        if len(value) != 10 or not value.isdigit():
             raise ValueError("The phone number must have 10 digits")
 
 
